0742c0ea96d2f8adbd11191d43c84edf24ff72e7
harry obaseki <harryobas@gmail.com>
2019-12-13 18:59:10 +0100

    devices controller - #index returns list of monitored devices

--- lib/device_toggler.rb
+++ lib/device_toggler.rb
@@ -1,12 +1,13 @@
 class DeviceToggler
   def self.toggle_device(device_id)
+    Device.update(device_id, attributes)
     device = Device.find_by_id(device_id)
     case device.status
     when "on"
       device.status = "off"
     else
       device.status = "on"
-    end 
+    end
     device.save
     device
   end
--- spec/controllers/devices_controller_spec.rb
+++ spec/controllers/devices_controller_spec.rb
@@ -29,22 +29,17 @@ RSpec.describe DevicesController, type: :controller do
   # Device. As you add validations to Device, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    {name: "tv", status: "on", current_consumption: "20w", location: "livingroom"}
   }
 
   let(:invalid_attributes) {
     skip("Add a hash of attributes invalid for your model")
   }
 
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # DevicesController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
-
   describe "GET #index" do
     it "returns a success response" do
       device = Device.create! valid_attributes
-      get :index, params: {}, session: valid_session
+      get :index, params: {}
       expect(response).to be_successful
     end
   end
@@ -52,37 +47,11 @@ RSpec.describe DevicesController, type: :controller do
   describe "GET #show" do
     it "returns a success response" do
       device = Device.create! valid_attributes
-      get :show, params: {id: device.to_param}, session: valid_session
+      get :show, params: {id: device.to_param}
       expect(response).to be_successful
     end
   end
 
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Device" do
-        expect {
-          post :create, params: {device: valid_attributes}, session: valid_session
-        }.to change(Device, :count).by(1)
-      end
-
-      it "renders a JSON response with the new device" do
-
-        post :create, params: {device: valid_attributes}, session: valid_session
-        expect(response).to have_http_status(:created)
-        expect(response.content_type).to eq('application/json')
-        expect(response.location).to eq(device_url(Device.last))
-      end
-    end
-
-    context "with invalid params" do
-      it "renders a JSON response with errors for the new device" do
-
-        post :create, params: {device: invalid_attributes}, session: valid_session
-        expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to eq('application/json')
-      end
-    end
-  end
 
   describe "PUT #update" do
     context "with valid params" do
@@ -117,13 +86,6 @@ RSpec.describe DevicesController, type: :controller do
     end
   end
 
-  describe "DELETE #destroy" do
-    it "destroys the requested device" do
-      device = Device.create! valid_attributes
-      expect {
-        delete :destroy, params: {id: device.to_param}, session: valid_session
-      }.to change(Device, :count).by(-1)
-    end
-  end
+
 
 end
