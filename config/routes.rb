5de5302e1cedb693565978e37c73fa35025bfc5d
harry obaseki <harryobas@gmail.com>
2019-12-15 14:26:22 +0100

    authentication controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :devices, only: [:index, :show, :update]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post 'authenticate', to: 'authentication#authenticate'
+
 end
